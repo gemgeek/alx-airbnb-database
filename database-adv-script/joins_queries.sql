2052ae868ab382c194a0e7fb2e4a35025a9f9017
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-03 21:16:46 +0100

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -7,6 +7,7 @@ INNER JOIN users u ON b.user_id = u.user_id;
 SELECT p.property_id, p.name AS property_name, r.review_id, r.rating, r.comment
 FROM properties p
 LEFT JOIN reviews r ON p.property_id = r.property_id;
+ORDER BY p.id;
 
 -- 3. FULL OUTER JOIN: Users and bookings (even if no match)
 SELECT u.user_id, u.name, b.booking_id, b.property_id
